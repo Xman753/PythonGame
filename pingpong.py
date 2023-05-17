108ca1a0f90c879e0023bb8d93ff5d5fa8caa65d
Xman753 <75484670+Xman753@users.noreply.github.com>
2023-05-17 19:32:34 +0300

    Add files via upload

--- pingpong.py
+++ pingpong.py
@@ -14,11 +14,14 @@ class GameSprite(sprite.Sprite):
         mw.blit(self.img, (self.rect.x, self.rect.y))
 class Player(GameSprite):
     def __init__(self, img, sizeX, sizeY, speed=0, id=0):
-        super().__init__(img, (maxX - sizeX)//2, maxY - sizeY, sizeX, sizeY, speed) 
+        if id == 1: 
+            super().__init__(img, 15, maxY - sizeY, sizeX, sizeY, speed)
+        else:
+             super().__init__(img, maxX - sizeX - 15, maxY - sizeY, sizeX, sizeY, speed)
         self.id = id       
     def keyProcessing(self):
         keys = key.get_pressed()
-        if self.id == 1:
+        if self.id == 2:
             if keys[K_UP]:
                 self.rect.y -= self.speed
             if keys[K_DOWN]:
@@ -35,6 +38,29 @@ class Player(GameSprite):
         elif self.rect.y < 0:
             self.rect.y = 0
 
+class Ball(GameSprite):
+    def __init__(self, img, x, y, sizeX, sizeY, speed):
+        super().__init__(img, x, y, sizeX, sizeY, speed)
+        self.speedX = (1 - 2 * (randint(0, 1))) * speed
+        self.speedY = (1 - 2 * (randint(0, 1))) * speed
+    def move(self):
+        self.rect.x += self.speedX
+        self.rect.y += self.speedY
+        if self.rect.y <= 0:
+            self.speedY *= -1
+            return 3
+        if self.rect.y >= maxY - self.rect.height:
+            self.speedY *= -1
+            return 4
+        if self.rect.x <= 0:
+            self.speedX *= -1
+            return 1
+        if self.rect.x >= maxX - self.rect.width:
+            self.speedX *= -1
+            return 2
+        return 0
+
+bgColor = (255, 255, 255)
 maxX = 1280
 maxY = 720
 selfPrevShotTime = tm()
@@ -48,11 +74,13 @@ fire = mixer.Sound('ppfire.ogg')#загрузить звук выстрела
 
 game = False
 clock = time.Clock()
-#bg = GameSprite('galaxy.jpg', 0, 0, maxX, maxY)
-playerL1 = Player('Plt1.png', 25, 100, 10, 1)
-playerR2 = Player('Plt2.png', 25, 100, 10, 2)
+playerL1 = Player('Plt1.png', 25, 100, 5, 1)
+playerR2 = Player('Plt2.png', 25, 100, 5, 2)
+ball = Ball('Ball.png', (maxX - 35)//2, (maxY - 35)//2, 35, 35, 5)
+
 font.init()
 font_ = font.SysFont('Arial', 50)
+
 victoryL = font_.render('Player 1 WON', True, (255, 0, 0))
 victoryR = font_.render('Player 2 WON', True, (255, 0, 0))
 
@@ -66,10 +94,18 @@ while game != True:
             game = False
 while game:
     if gameRes == 0:
+        mw.fill(bgColor)
         playerL1.keyProcessing()         
         playerL1.reset()
         playerR2.keyProcessing()         
         playerR2.reset()
+        gameRes = ball.move()
+        if gameRes > 2:
+            gameRes = 0
+        if sprite.collide_rect(ball, playerL1) or sprite.collide_rect(ball, playerR2):
+            ball.speedX *= -1
+            fire.play()
+        ball.reset()
     elif gameRes == 1:
         mw.blit(victoryL, (maxX//2, maxY//2))
     elif gameRes == 2:
@@ -78,4 +114,4 @@ while game:
         if e.type == QUIT:
             game = False
     display.update()
-    clock.tick(9999999999999999999999999999999999)
+    clock.tick(50)
